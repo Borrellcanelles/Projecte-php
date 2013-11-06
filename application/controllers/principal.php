4ee1008726f12ed019de88d08fac461ab10aecff
josepborrellsanchez <josepborrellsanchez@iesebre.com>
2013-11-06 19:29:13 +0100

    updating i deleting

--- application/controllers/principal.php
+++ application/controllers/principal.php
@@ -12,7 +12,7 @@ class principal extends CI_Controller {
 	public function principal1()
 	{
 		$this->load->view('principal'); 
-		$query = $this->db->get('actor');
+		$query = $this->db->query('SELECT * FROM actor LIMIT 10');
 		if ($query->num_rows() > 0) {
 			foreach ($query->result_array() as $row) {
 				echo $row['actor_id']. $row['fullname']. "<br>";
@@ -25,9 +25,26 @@ class principal extends CI_Controller {
 		'actor_id' => 9141 ,
 		'fullname' => 'Mi nombre' ,
             );
-            
             $this->db->insert('actor', $data); 
 		}
+		
+		/*public function provaborrar(){
+			$id = 9141;
+			$this->db->where('actor_id', $id);
+			$this->db->delete('actor'); 
+			}*/
+			
+			public function provaupdate(){
+			$data = array(
+               'actor_id' => 201
+            );
+			$id = 200;
+			$idnou = 201;
+			$this->db->where('actor_id', $id);	
+			$this->db->update('actor',$data); 
+			
+			
+		//DELETE FROM `users`.`actor` WHERE `actor`.`actor_id` = 9141
 		
 
 	/*
@@ -38,5 +55,8 @@ class principal extends CI_Controller {
     }
     $objeto = new Myclass;
     $this->db->insert('actors', $objeto);
+    * 
+    * return $this->db->affected_rows() > 0;
 */
+}
 }
