4d57e03fc63ff0bb5cdb6b3912f0309a3414175f
josepborrellsanchez <josepborrellsanchez@iesebre.com>
2014-09-23 17:33:32 +0200

    NOU SQL

--- projecte.sql
+++ projecte.sql
@@ -1,25 +1,27 @@
 CREATE TABLE Llibres
 (
-ISBN varchar(13),
-Titol varchar(100),
-Autor varchar(100),
+ISBN varchar(13) NOT NULL,
+Titol varchar(100) NOT NULL,
+Autor varchar(100) NOT NULL,
 Stock_actual int,
-PVP float,
+PVP float NOT NULL,
 Any_de_publicacio int,
-Editorial varchar(100),
-Tematica varchar(100)
-); 
+Editorial varchar(100) NOT NULL,
+Tematica varchar(100),
+PRIMARY KEY (ISBN));
 
 CREATE TABLE Clients (
-DNI varchar(9),
-Nom varchar(100),
-Cognoms varchar(100),
-Adreça varchar(100),
-Telefon int,
-Comandes_realitzades int  );
+DNI varchar(9) NOT NULL,
+Nom varchar(100) NOT NULL,
+Cognoms varchar(100) NOT NULL,
+Adreça varchar(100) NOT NULL,
+Telefon int NOT NULL,
+Comandes_realitzades int,
+PRIMARY KEY (DNI));
 
 CREATE TABLE Ventes (
-ID int,
+ID int AUTO_INCREMENT ,
 DNI varchar(9),
-ISBN varchar(13));
+ISBN varchar(13),
+PRIMARY KEY (ID));
 
